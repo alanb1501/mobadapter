a554bce207e74da445ca508ff9c82bc68960a325
Alan <balasuar@gmail.com>
2013-03-09 17:50:07 -0800

    adding end to end to mob

--- adapter.js
+++ adapter.js
@@ -2,6 +2,41 @@
 var soap = require('soap');
 var nconf = require('nconf');
 var express = require('express');
+var uuid  = require('uuid');
+
+var User = function (fname,lname,addLine1,addLine2,city,state,country,zip,phone,email) {
+    //todo: pull this formatting logic into a more appropriate/centralized place
+    this.id = uuid.v4();
+    this.fname = fname;
+    this.lname = lname;
+    this.addressLine1 = addLine1;
+    this.addressLine2 = addLine2;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phone = phone;
+    this.email = email;
+}
+
+User.prototype = {
+    serialize: function () {
+        return {
+            "ID": this.id,
+            "FirstName": this.fname,
+            "LastName":this.lname,
+            "Email":this.email,
+            "AddressLine1":this.addressLine2,
+            "AddressLine2":this.addressLine2,
+            "City":this.city,
+            "State":this.state,
+            "PostalCode":this.zip,
+            "Country":this.country,
+            "MobilePhone":this.phone,
+            "BirthDate":"1980-01-01",
+            "ReferredBy":"Web"
+        };
+    }
+}
 
 
 function die(message) {
@@ -9,19 +44,22 @@ function die(message) {
 	process.exit();
 }
 
+function getSourceCreds() {
+	return 	{ 	
+		"SourceName": nconf.get('source'),
+		"Password": nconf.get('key'),
+		"SiteIDs": [
+			{"int":nconf.get('siteid')}
+	]};
+}
+
 function activate(client) {
 	client.GetActivationCode(
 	{
-		"Request":{
-			"SourceCredentials": {
-			"SourceName": nconf.get('source'),
-			"Password": nconf.get('key'),
-			"SiteIDs": [
-				{"int":nconf.get('siteid')}
-			]
+		"Request": {
+			"SourceCredentials" : getSourceCreds()
 		}
-	}
-
+		
 	},function(err,result){
 
 		if(err && err != null) {
@@ -35,6 +73,23 @@ function activate(client) {
 	});
 }
 
+
+
+function addUser(client,user) {
+	client.AddOrUpdateClients({
+		"Request": {
+			"SourceCredentials" : getSourceCreds(),
+			"Clients": [{
+				"Client":user.serialize()
+			}]
+		}
+	},function(err,results) {
+		console.log(results);
+		//console.log(JSON.stringify(results));
+		process.exit();
+	});
+}
+
 //Load Global configurations
 nconf.argv().file('global','./global.json');
 
@@ -43,11 +98,10 @@ nconf.file('user',nconf.get('user') || './user.json').env();
 
 if(nconf.get('help')) {
 	console.log(process.argv.join(' '));
-	console.log('usage:');
+	console.log('usage:');ae
 	console.log('	--help: this help message');
 	console.log('	--user /path/to/user.json');
 	console.log('	--activate: outputs activation code');
-
 }
 
 if(!nconf.get('key') || !nconf.get('source')) {
@@ -61,13 +115,42 @@ if(nconf.get('activate')) {
 	});
 }
 
+if(nconf.get('testadduser')) {
+	soap.createClient(nconf.get('clientWsdl'),function(err,client) {
+		//var user = function (fname,lname,addline1,addline2,city,state,country,zip,phone,email) {
+		var u = new User("arbTest","arbTest2","1234 Anywhere","Suite 187","Renton","WA","USA","98057","5554143145","test@tempuri.org");
+		console.log(u.serialize());
+		addUser(client,u);
+	});
+}
+
+
 // create a daemon
 var app = express();
 app.use(express.bodyParser());
 
 app.post('/wufoo/adduser',function(req,res) {
-	console.log(req.body);
-	res.send(req.body);
+	//todo: reduce this.
+	var fName = req.body[nconf.get('firstName')];
+	var lName = req.body[nconf.get('lastName')];
+	var addLine1 = req.body[nconf.get('addressLine1')];
+	var addLine2 = req.body[nconf.get('addressLine2')];
+	var city = req.body[nconf.get('city')];
+	var state = req.body[nconf.get('state')];
+	var country = req.body[nconf.get('country')];
+	var zip = req.body[nconf.get('zipCode')];
+	var email = req.body[nconf.get('email')];
+	var phone = req.body[nconf.get('phone')];
+	var rsvp = req.body[nconf.get('rsvpClass')];
+	var referral = req.body[nconf.get('referral')];
+	res.send('ok!');
+
+	soap.createClient(nconf.get('clientWsdl'),function(err,client) {
+		//var user = function (fname,lname,addline1,addline2,city,state,country,zip,phone,email) {
+		var u = new User(fName,lName,addLine1,addLine2,city,state,country,zip,phone,email);
+		console.log(u.serialize());
+		addUser(client,u);
+	});
 });
 
 app.get('*',function(req,res){
